f28701a5643c6a63a02c839ef54d862196dc282c
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-31 15:25:26 -0500

    Fix pre-genned controller tests

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user1 = User.create(email:"example1@example.com",password:"password",password_confirmation:"password",role_id:1)
+    sign_in @user1
+  end
   test "should get index" do
-    get rooms_index_url
+    get rooms_path
     assert_response :success
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user1 = User.create(id:1,email:"example1@example.com",password:"password",password_confirmation:"password",role_id:1)
+    sign_in @user1
+    @user2 = User.create(id:2,email:"example2@example.com",password:"password",password_confirmation:"password",role_id:1)
+  end
   test "should get show" do
-    get users_show_url
+    get user_url(@user2)
     assert_response :success
   end
 end
