4b09d7d8d43d8d806a42cb91c130d4f86b9e7844
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-31 10:55:42 -0500

    Initialize html markup for chat messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,16 @@
 module MessagesHelper
+  def markdown(text)
+    options = {
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: '_blank' },
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      superscript: true,
+      strikethrough: true,
+      underline: true
+    }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    sanitize(markdown.render(text))
+  end
 end
