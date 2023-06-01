222f444b0705cb77755dacd5d2cc0b735da27653
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-06-01 12:20:08 -0500

    Test message and participant creation

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user1 = User.first
+    @user2 = User.find(2)
+    @admin = User.find(3)
+    @room = Room.create(name:"messagetestroom",is_private:false)
+  end
+
+  test "messages created successfully" do
+    message1 = @user1.messages.create(body:"test1", room_id:@room.id)
+    message2 = @user1.messages.create(body:"test2", room_id:@room.id)
+    assert_equal @room.messages.size, 2
+  end
+
+  test "admin can read messages but should not be able to send one in pm" do
+    @dm = Room.create_private_room([@user1, @user2], "private_1_2")
+    message1 = @user1.messages.create(body:"test1", room_id:@dm.id)
+    message2 = @user2.messages.create(body:"test2", room_id:@dm.id)
+    # testing that admin can access room elsewhere. this just tests that the admin cannot dm
+    message3 = @admin.messages.create(body:"test3", room_id:@dm.id)
+    assert_equal @dm.messages.size, 2
+  end
+
 end
--- test/models/participant_test.rb
+++ test/models/participant_test.rb
@@ -1,5 +1,28 @@
 require "test_helper"
 
 class ParticipantTest < ActiveSupport::TestCase
-  
+
+  setup do
+  @user1 = User.create(id:1,email:"example1@example.com",password:"password",password_confirmation:"password",role_id:1)
+  @user2 = User.create(id:2,email:"example2@example.com",password:"password",password_confirmation:"password",role_id:1)
+
+  @room1 = Room.create_private_room([@user1, @user2], "private_1_2")
+  end
+
+  test "private chat should have two participants" do
+    assert_equal @room1.participants.size, 2
+  end
+
+  test "private chat should have user1 as a participant" do
+    assert_equal @room1.participants.where("user_id = '#{@user1.id}'").first.user, @user1
+  end
+
+  test "private chat should have user2 as a participant" do
+    assert_equal @room1.participants.where("user_id = '#{@user2.id}'").first.user, @user2
+  end
+
+  test "private chat should have different users as participants" do
+    assert_not_equal @room1.participants.first, @room1.participants.last
+  end
+
 end
